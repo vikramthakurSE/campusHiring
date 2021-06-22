e7608ae63e98b9f8586018aad076ec5fc0e62683
Vikram <vikramthakur381@gmail.com>
2021-06-22 13:41:04 +0530

    First commit with navbar

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.listen(4000, function(){
+    console.log("Server is started !!");
+})
