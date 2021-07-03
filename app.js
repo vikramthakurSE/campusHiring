b91ac693c77d00a8515f9c545bc0d51efb13fce2
Vikram <vikramthakur381@gmail.com>
2021-07-03 21:56:57 +0530

    admin page UI Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Student = require("./modules/registration");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 
